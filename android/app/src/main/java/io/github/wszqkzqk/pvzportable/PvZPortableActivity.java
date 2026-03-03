c394e96ee77b64759d9250d3753f281cadd171d4
Zhou Qiankang <wszqkzqk@qq.com>
2026-03-03 20:53:59 +0800

    Feat: Implement system UI hiding for immersive experience on Android
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- android/app/src/main/java/io/github/wszqkzqk/pvzportable/PvZPortableActivity.java
+++ android/app/src/main/java/io/github/wszqkzqk/pvzportable/PvZPortableActivity.java
@@ -1,8 +1,14 @@
 package io.github.wszqkzqk.pvzportable;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
 
 import org.libsdl.app.SDLActivity;
 
@@ -18,12 +24,40 @@ public class PvZPortableActivity extends SDLActivity {
         Log.i(TAG, "Resource dir: " + (extDir != null ? extDir.getAbsolutePath() : "null"));
 
         if (!hasGameResources(extDir)) {
+            super.onCreate(savedInstanceState);
             startActivity(new Intent(this, ResourceImportActivity.class));
             finish();
             return;
         }
 
         super.onCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) hideSystemUI();
+    }
+
+    private void hideSystemUI() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            Window window = getWindow();
+            WindowInsetsController controller = window.getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+                controller.setSystemBarsBehavior(
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        }
     }
 
     @Override
